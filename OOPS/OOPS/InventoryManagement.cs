55b30d1c8eae23fdf3a036751146fe62b57ea8e6
Sikander <sikku.rk405@gmail.com>
2022-02-07 21:29:16 +0530

    [Sikander] Added json inventory for list of grocery items program

---
+++ OOPS/OOPS/InventoryManagement.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    public class InventoryManagement
+    {
+        public void ReadData(string filepath)
+        {
+            try
+            {
+                // var json = JsonConvert.DeserializeObject<Inventory>(filepath);
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    var json = reader.ReadToEnd();
+                    var items = JsonConvert.DeserializeObject<List<Inventory>>(json);
+                    Console.WriteLine("name\t" + "price\t" + "weight\t" + "total amount");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item.Name + "\t" + item.Price + "\t" + item.Weight + "\t" + item.Price * item.Weight);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ OOPS/OOPS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryManagement inventoryManagement = new InventoryManagement();
+            inventoryManagement.ReadData(@"C:\bridgelabz\OOPS\OOPS\OOPS\Inventory.json");
+            Console.ReadKey();
+        }
+    }
+}
